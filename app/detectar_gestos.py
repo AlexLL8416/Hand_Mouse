7fa72baa52429da8f421257be1a8464837e3480a
Alejandro Lara Lara <alejandroll8416@gmail.com>
2025-09-23 01:10:47 +0200

    Umbrales normalizados

--- app/detectar_gestos.py
+++ app/detectar_gestos.py
@@ -5,5 +5,40 @@
 # Simplemente calculo su distancia euclidea con el módulo math
 import math
 
-#def analizarDistancias(lista_posicones):
-    #
+def analizarDistancias(lista_posicones):
+    res = []
+    for i in range(0,len(lista_posicones),2):
+        res.append(math.dist(lista_posicones[i],lista_posicones[i+1]))
+    ref = math.dist(lista_posicones[3], lista_posicones[9]) # índice_mcp y meñique_mcp
+    return [d/ref for d in res]
+
+# Umbrales para considerar cada dedo "cerrado"
+UMBRAL_DEDOS = {
+    "pulgar": 0.7,
+    "indice": 0.5,
+    "corazon": 0.4,
+    "anular": 0.35,
+    "menique": 0.35,
+}
+
+def dedoCerrado(lista_posiciones, dedo):
+    """Devuelve True si el dedo indicado está cerrado."""
+    distancias = analizarDistancias(lista_posiciones)
+    # usamos el orden de UMBRAL_DEDOS.keys() para asignar índices
+    dedos = list(UMBRAL_DEDOS.keys())
+    idx = dedos.index(dedo)
+    return distancias[idx] <= UMBRAL_DEDOS[dedo] #True si dedo está cerrado
+
+def manoAbierta(lista_posiciones):
+    """Devuelve True si todos los dedos están abiertos."""
+    return all(
+        not dedoCerrado(lista_posiciones, dedo)
+        for dedo in UMBRAL_DEDOS
+    )
+
+def manoCerrada(lista_posiciones):
+    """Devuelve True si todos los dedos están cerrados."""
+    return all(
+        dedoCerrado(lista_posiciones, dedo)
+        for dedo in UMBRAL_DEDOS
+    )
--- app/detectar_manos.py
+++ app/detectar_manos.py
@@ -2,6 +2,7 @@ import cv2
 import numpy
 import mediapipe
 import math
+import detectar_gestos
 
 mp_hands = mediapipe.solutions.hands
 mp_drawing= mediapipe.solutions.drawing_utils
@@ -36,8 +37,6 @@ with mp_hands.Hands(
                 x1=int(hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_MCP].x*width)
                 y1=int(hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_MCP].y*height)
                 posiciones.append((x1,y1))
-                print("Empieza mano")
-                print(math.dist((x4,y4),(x1,y1)))
                 #Indice
                 x8=int(hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x*width)
                 y8=int(hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y*height)
@@ -45,7 +44,6 @@ with mp_hands.Hands(
                 x5=int(hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_MCP].x*width)
                 y5=int(hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_MCP].y*height)
                 posiciones.append((x5,y5))
-                print(math.dist((x8,y8),(x5,y5)))
                 #Corazón
                 x12=int(hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].x*width)
                 y12=int(hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y*height)
@@ -53,7 +51,6 @@ with mp_hands.Hands(
                 x9=int(hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_MCP].x*width)
                 y9=int(hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_MCP].y*height)
                 posiciones.append((x9,y9))
-                print(math.dist((x12,y12),(x9,y9)))
                 #Anular
                 x16=int(hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_TIP].x*width)
                 y16=int(hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_TIP].y*height)
@@ -61,7 +58,6 @@ with mp_hands.Hands(
                 x13=int(hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_MCP].x*width)
                 y13=int(hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_MCP].y*height)
                 posiciones.append((x13,y13))
-                print(math.dist((x16,y16),(x13,y13)))
                 #Meñique
                 x20=int(hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_TIP].x*width)
                 y20=int(hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_TIP].y*height)
@@ -69,13 +65,7 @@ with mp_hands.Hands(
                 x17=int(hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_MCP].x*width)
                 y17=int(hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_MCP].y*height)
                 posiciones.append((x17,y17))
-                print(math.dist((x20,y20),(x17,y17)))
-                print("Fin mano")
-                #print(posiciones)
-                #indice = math.dist((x8,y8),(x5,y5))
-                #print(indice)
-                #if indice <= 24:
-                #    print("índice cerrado")
+                print(detectar_gestos.manoCerrada(posiciones))
                 mp_drawing.draw_landmarks(frame_invertido, hand_landmarks, mp_hands.HAND_CONNECTIONS)
                 cv2.circle(frame_invertido, (x8,y8),3,(255,0,0),3)
                 cv2.circle(frame_invertido, (x5,y5),3,(0,255,0),3)
